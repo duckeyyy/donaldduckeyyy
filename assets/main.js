79e50cd859890b6d12b653b9a88ee0f211082a13
duckeyyy <121089927+duckeyyy@users.noreply.github.com>
2023-01-02 12:20:26 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -14,7 +14,7 @@ yes.addEventListener('click', () => {
 	con.style.backgroundColor = '#f27b7b';
 	con.style.boxShadow = '#d95454 15px 15px 20px inset, #d95454 -15px -15px 20px inset, #cf1e1e 0 0 100px';
 
-	txt.innerHTML = 'Bat di mo alammm HAHAHAHAHA';
+	txt.innerHTML = 'NOW NALANG KASIII HAHAHAHAHA';
 
 	txt.style.color = 'fff';
 	txt.style.textShadow = '1px 1px #f44d4d, -1px -1px #f444d4d, 0 0 20px red';
@@ -35,7 +35,7 @@ no.addEventListener('click', () => {
 	con.style.backgroundColor = '#f27b7b';
 	con.style.boxShadow = '#d95454 15px 15px 20px inset, #d95454 -15px -15px 20px inset, #cf1e1e 0 0 100px';
 
-	txt.innerHTML = 'HAHAHAHAAHAHAH HATDOGGGG';
+	txt.innerHTML = 'HAHAHAHAAHAHAH KINIKILIG AKO SHSDJSJSDHSHDSAKSJSDJ';
 
 	txt.style.color = 'fff';
 	txt.style.textShadow = '1px 1px #f44d4d, -1px -1px #f444d4d, 0 0 20px red';
